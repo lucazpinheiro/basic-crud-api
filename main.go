30e6e92ca9911cbadd7480030448e2674d19847d
Lucas <lucascpinheiro10@gmail.com>
2023-07-27 22:56:59 -0300

    crud operations

--- main.go
+++ main.go
@@ -19,11 +19,11 @@ const (
 )
 
 type Product struct {
-	ID          int
-	Name        string
-	Available   bool
-	Price       float64
-	Description string
+	ID          int     `json:",omitempty"`
+	Name        string  `json:",omitempty"`
+	Available   bool    `json:",omitempty"`
+	Price       float64 `json:",omitempty"`
+	Description string  `json:",omitempty"`
 }
 
 func main() {
@@ -41,7 +41,7 @@ func main() {
 	app.Get("/products", func(c *fiber.Ctx) error {
 		var product Product
 		var products []Product
-		sql := `SELECT id, name, available, price, description FROM public.products`
+		sql := `SELECT id, name, isAvailable, price, description FROM public.products`
 		rows, err := db.Query(sql)
 		if err != nil {
 			log.Fatal(err)
@@ -65,7 +65,7 @@ func main() {
 		id := c.Params("id")
 
 		var product Product
-		sql := `SELECT id, name, available, price, description FROM public.products WHERE id = $1`
+		sql := `SELECT id, name, isAvailable, price, description FROM public.products WHERE id = $1`
 		row := db.QueryRow(sql, id)
 
 		err := row.Scan(&product.ID, &product.Name, &product.Available, &product.Price, &product.Description)
@@ -94,12 +94,44 @@ func main() {
 		return c.JSON(&p.ID)
 	})
 
-	app.Patch("/products/:id", func(c *fiber.Ctx) error {
-		return c.SendString("TODO: update product")
+	app.Put("/products/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		var p Product
+
+		var productID int
+		selectSQL := `SELECT id FROM public.products WHERE id = $1`
+		err := db.QueryRow(selectSQL, id).Scan(&productID)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON("An error occured")
+		}
+		if err := c.BodyParser(&p); err != nil {
+			log.Fatal(err)
+			c.JSON("An error occured")
+		}
+
+		updateSQL := `UPDATE public.products SET name = $1, isAvailable = $2, price = $3, description = $4 WHERE id = $5`
+		_, err = db.Exec(updateSQL, p.Name, p.Available, p.Price, p.Description, id)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON("An error occured")
+		}
+
+		return c.JSON(p)
 	})
 
 	app.Delete("/products/:id", func(c *fiber.Ctx) error {
-		return c.SendString("TODO: delete product")
+		id := c.Params("id")
+		var deletedID int
+
+		sql := `DELETE FROM public.products WHERE id = $1`
+		err := db.QueryRow(sql, id).Scan(&deletedID)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON("An error occured")
+		}
+
+		return c.JSON(deletedID)
 	})
 
 	app.Listen(":3000")
