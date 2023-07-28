ce096ca357076d7effe016649df2f70d25787d7d
Lucas <lucascpinheiro10@gmail.com>
2023-07-27 23:11:23 -0300

    minor adjustment

--- main.go
+++ main.go
@@ -39,99 +39,23 @@ func main() {
 	})
 
 	app.Get("/products", func(c *fiber.Ctx) error {
-		var product Product
-		var products []Product
-		sql := `SELECT id, name, isAvailable, price, description FROM public.products`
-		rows, err := db.Query(sql)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			err := rows.Scan(&product.ID, &product.Name, &product.Available, &product.Price, &product.Description)
-			if err != nil {
-				log.Fatal(err)
-				c.JSON("An error occured")
-			}
-			products = append(products, product)
-		}
-
-		return c.JSON(products)
+		return getHandler(c, db)
 	})
 
 	app.Get("/products/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		var product Product
-		sql := `SELECT id, name, isAvailable, price, description FROM public.products WHERE id = $1`
-		row := db.QueryRow(sql, id)
-
-		err := row.Scan(&product.ID, &product.Name, &product.Available, &product.Price, &product.Description)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-
-		return c.JSON(product)
+		return getByIDHandler(c, db)
 	})
 
 	app.Post("/products", func(c *fiber.Ctx) error {
-		p := new(Product)
-
-		if err := c.BodyParser(p); err != nil {
-			return err
-		}
-
-		sql := `INSERT INTO public.products (name, isAvailable, price, description) VALUES ($1, $2, $3, $4) RETURNING id`
-		err := db.QueryRow(sql, p.Name, p.Available, p.Price, p.Description).Scan(&p.ID)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-
-		return c.JSON(&p.ID)
+		return postHandler(c, db)
 	})
 
 	app.Put("/products/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		var p Product
-
-		var productID int
-		selectSQL := `SELECT id FROM public.products WHERE id = $1`
-		err := db.QueryRow(selectSQL, id).Scan(&productID)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-		if err := c.BodyParser(&p); err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-
-		updateSQL := `UPDATE public.products SET name = $1, isAvailable = $2, price = $3, description = $4 WHERE id = $5`
-		_, err = db.Exec(updateSQL, p.Name, p.Available, p.Price, p.Description, id)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-
-		return c.JSON(p)
+		return putHandler(c, db)
 	})
 
 	app.Delete("/products/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		var deletedID int
-
-		sql := `DELETE FROM public.products WHERE id = $1`
-		err := db.QueryRow(sql, id).Scan(&deletedID)
-		if err != nil {
-			log.Fatal(err)
-			c.JSON("An error occured")
-		}
-
-		return c.JSON(deletedID)
+		return deleteHandler(c, db)
 	})
 
 	app.Listen(":3000")
@@ -145,4 +69,100 @@ func OpenConn() (*sql.DB, error) {
 	db, err := sql.Open("postgres", psqlInfo)
 
 	return db, err
+}
+
+func getHandler(c *fiber.Ctx, db *sql.DB) error {
+	var product Product
+	var products []Product
+	sql := `SELECT id, name, isAvailable, price, description FROM public.products`
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&product.ID, &product.Name, &product.Available, &product.Price, &product.Description)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON("An error occured")
+		}
+		products = append(products, product)
+	}
+
+	return c.JSON(products)
+}
+
+func getByIDHandler(c *fiber.Ctx, db *sql.DB) error {
+	id := c.Params("id")
+
+	var product Product
+	sql := `SELECT id, name, isAvailable, price, description FROM public.products WHERE id = $1`
+	row := db.QueryRow(sql, id)
+
+	err := row.Scan(&product.ID, &product.Name, &product.Available, &product.Price, &product.Description)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+
+	return c.JSON(product)
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	p := new(Product)
+
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+
+	sql := `INSERT INTO public.products (name, isAvailable, price, description) VALUES ($1, $2, $3, $4) RETURNING id`
+	err := db.QueryRow(sql, p.Name, p.Available, p.Price, p.Description).Scan(&p.ID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+
+	return c.JSON(&p.ID)
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	id := c.Params("id")
+	var p Product
+
+	var productID int
+	selectSQL := `SELECT id FROM public.products WHERE id = $1`
+	err := db.QueryRow(selectSQL, id).Scan(&productID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+	if err := c.BodyParser(&p); err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+
+	updateSQL := `UPDATE public.products SET name = $1, isAvailable = $2, price = $3, description = $4 WHERE id = $5`
+	_, err = db.Exec(updateSQL, p.Name, p.Available, p.Price, p.Description, id)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+
+	return c.JSON(p)
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	id := c.Params("id")
+	var deletedID int
+
+	sql := `DELETE FROM public.products WHERE id = $1`
+	err := db.QueryRow(sql, id).Scan(&deletedID)
+	if err != nil {
+		log.Fatal(err)
+		c.JSON("An error occured")
+	}
+
+	return c.JSON(deletedID)
 }
